a43f3e7d72708e9ad53fffb2a4a776904be5884e
Ricardo.ayala-g <Ricardo.ayala-g@mail.escuelaing.edu.co>
2025-05-23 15:17:52 -0500

    Inicio

---
+++ src/main/java/com/cvds/parcial/back_parcial_3/BackParcial3Application.java
@@ -0,0 +1,13 @@
+package com.cvds.parcial.back_parcial_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackParcial3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackParcial3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvds/parcial/back_parcial_3/BackParcial3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvds.parcial.back_parcial_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackParcial3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
